b9722f84dfc0f98d814faa2619f23d26a50e0891
cs <bartus.csongor@gmail.com>
2014-09-18 19:55:56 +0300

    CMS is also clickable

--- assets/scripts/jquery.init.js
+++ assets/scripts/jquery.init.js
@@ -4,5 +4,10 @@ $(document).ready(function() {
   $('.frontpage article.clickable').click(function() {
     $(this).find('aside').slideToggle('slow');
   });
+  
+  // CMS
+  $('.frontpage #cms article h3').click(function() {
+    $(this).next('aside').slideToggle('slow');
+  });
 });
 
